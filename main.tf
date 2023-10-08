95e14551feb55207ee796824e1fd04b179afc19f
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-08 15:20:07 +0530

    Ansible

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_ssm_parameter" "params" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  name = var.parameters[count.index].type
+  name = var.parameters[count.index].value
+}
+
+variable "parameters" {
+  default = [
+    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
+    {name = "prod.rds.master_username", value = "expense", type = "String"},
+  ]
+}
+
+
