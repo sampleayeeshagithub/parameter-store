9d9b6c42b53266b0be19d50d2383510eb2351994
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-16 16:10:44 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -32,6 +32,10 @@ variable "parameters" {
     { name = "dev.roboshop.frontend.user_url", value = "http://user-dev.ayeeshadevops75.online:8080/", type = "String" },
     { name = "dev.roboshop.frontend.shipping_url", value = "http://shipping-dev.ayeeshadevops75.online:8080/", type = "String" },
     { name = "dev.roboshop.frontend.payment_url", value = "http://payment-dev.ayeeshadevops75.online:8080/", type = "String" },
+    { name = "dev.roboshop.catalogue.MONGO_URL", value = "mongodb://mongodb-dev.ayeeshadevops75.online:27017/catalogue", type = "String" },
+    { name = "dev.roboshop.catalogue.MONGO_ENDPOINT", value = "mongodb-dev.ayeeshadevops75.online", type = "String" },
+
+
   ]
 }
 
